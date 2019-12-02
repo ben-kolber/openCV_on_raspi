b32bcb22fa07e0fc3013892ad9ada25ae06810b0
booleanlogic21 <46203448+booleanlogic21@users.noreply.github.com>
2019-12-02 23:06:53 +0800

    Update raspi_camera.py

--- raspi_camera.py
+++ raspi_camera.py
@@ -23,42 +23,42 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
     	face_locations = face_recognition.face_locations(image)
 
-   	 # locate center of face
-   	 count = 0
-   	 for face_location in face_locations:
-    	    # Print the location of each face in this image\n",
-    	    top, right, bottom, left = face_location
-    	    print("FACE NUM {} LOCATED".format(count))
-    	    count += 1
-    	    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(
-       		     top, left, bottom, right))
+   	# locate center of face
+   	count = 0
+	for face_location in face_locations:
+    	 	# Print the location of each face in this image\n",
+		top, right, bottom, left = face_location
+    	    	print("FACE NUM {} LOCATED".format(count))
+    	    	count += 1
+    	   	print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(
+       			     top, left, bottom, right))
 
-      	  center_face_Y = (int)(top + ((bottom - top) / 2))
-      	  center_face_X = (int)(left + ((right - left) / 2))
+      	 	center_face_Y = (int)(top + ((bottom - top) / 2))
+      	 	center_face_X = (int)(left + ((right - left) / 2))
 	
-      	  print("located at AXIS X: {} | AXIS Y: {}".format(center_face_X, center_face_Y))
-      	  print('-' * 30)
-      	  print(' ' * 30)
-     	   # draw rectangle on image \n",
-      	  image = cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 2)
+      	  	print("located at AXIS X: {} | AXIS Y: {}".format(center_face_X, center_face_Y))
+      	  	print('-' * 30)
+      	  	print(' ' * 30)
+     	   	# draw rectangle on image \n",
+      	  	image = cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 2)
 
-      	  # draw a rectangle around center face
-      	  image = cv2.rectangle(image, ((center_face_X - 1), (center_face_Y - 1)),
+      	  	# draw a rectangle around center face
+      	  	image = cv2.rectangle(image, ((center_face_X - 1), (center_face_Y - 1)),
        	                       ((center_face_X + 1), (center_face_Y + 1)), (0, 255, 255), 5)
 
-   	 pil_image = Image.fromarray(image)
-    	 pil_image.show()
+   	 	pil_image = Image.fromarray(image)
+    	 	pil_image.show()
 
-    # using openCV
-    '''
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-    '''
+    		# using openCV
+   		 '''
+		# show the frame
+		cv2.imshow("Frame", image)
+		key = cv2.waitKey(1) & 0xFF
+    		'''
 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+		# clear the stream in preparation for the next frame
+		rawCapture.truncate(0)
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
